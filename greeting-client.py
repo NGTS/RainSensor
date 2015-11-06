17cb5cb48a2b68f5c404f3bdb0541d194f7f8659
jmccormac01 <jmccormac001@gmail.com>
2015-11-06 06:01:19 -0300

    tweaking old physical pins 3 and 5 to 12 and 16. pull-up resistors were killing booing process somehow...

--- greeting-client.py
+++ greeting-client.py
@@ -1,7 +1,7 @@
 # saved as greeting-client.py
 import Pyro4
 
-name = input("What is your name? ").strip()
+# name = input("What is your name? ").strip()
 
-greeting_maker = Pyro4.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
-print(greeting_maker.get_fortune(name))
+greeting_maker = Pyro4.Proxy("PYRONAME:example.sensor")    # use name server object lookup uri shortcut
+print(greeting_maker.get_fortune('test'))
--- greeting-server.py
+++ greeting-server.py
@@ -3,11 +3,11 @@ import Pyro4
 from collections import defaultdict
 import RPi.GPIO as g
 
-class GreetingMaker(object):
+class RainSensor(object):
     def get_fortune(self, name):
 		rs=defaultdict(list)
 		g.setmode(g.BCM)
-		gpio_nums=[2,3,4,17,27,22,10,9,11,5,6,13,19,26,20,21]
+		gpio_nums=[18,23,4,17,27,22,10,9,11,5,6,13,19,26,20,21]
 		for i in gpio_nums:
 			g.setup(i,g.IN)
 		for i in range(0,len(gpio_nums)):
@@ -16,8 +16,11 @@ class GreetingMaker(object):
 
 daemon = Pyro4.Daemon("10.2.5.107")                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
-ns.register("example.greeting", uri)   # register the object with a name in the name server
+uri = daemon.register(RainSensor)   # register the greeting maker as a Pyro object
+ns.register("example.sensor", uri)   # register the object with a name in the name server
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
+
+# start name server with
+# python -m Pyro4.naming -n 10.2.5.107
