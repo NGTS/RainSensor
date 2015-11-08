160288f2c1d303eab8afc4c9371316a52ccb2d31
jmccormac01 <jmccormac001@gmail.com>
2015-11-08 18:35:13 -0300

    added plotting of rain sensors

--- checkRainSensor.py
+++ checkRainSensor.py
@@ -1,33 +1,79 @@
 #!/usr/local/python/bin/python
 import pymysql
 from collections import defaultdict
+from astropy.time import Time
+import matplotlib.pyplot as pl
 
-def checkRainSensor():
+colours=[(206/250.,200/250.,170/250.),
+		(203/250.,81/250.,205/250.),
+		(213/250.,89/250.,52/250.),
+		(135/250.,215/250.,83/250.),
+		(210/250.,76/250.,121/250.),
+		(208/250.,180/250.,70/250.),
+		(99/250.,140/250.,75/250.),
+		(128/250.,121/250.,213/250.),
+		(120/250.,153/250.,179/250.),
+		(167/250.,118/250.,92/250.),
+		(120/250.,214/250.,181/250.),
+		(193/250.,128/250.,174/250.)]
+
+def argParse():
+	description='''Script to check the rain sensor calibration and plot the sensor values'''
+	parser=ap.ArgumentParser(description=description)
+	parser.add_argument('--check',type=int,help="check the sensors for last X hours")
+	parser.add_argument('--plot',type=int,help="plot sensor values for last X hours")
+	return parser.parse_args()
+
+def getLastXhrs(tlim):
 	conn=pymysql.connect(host='ds',db='ngts_ops')
 	cur=conn.cursor()
 	rs=defaultdict(list)
-	qry="SELECT * FROM rpi_rain_sensor"
+	times=[]
+	qry="SELECT (UNIX_TIMESTAMP(tsample)-UNIX_TIMESTAMP(UTC_TIMESTAMP()))/60.0 AS trel,rs01,rs02,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03,rs03 FROM rpi_rain_sensor HAVING trel>-%d" % (tlim)
 	cur.execute(qry)
 	for row in cur:
-		rs[1].append(row[2])
-		rs[2].append(row[3])
-		rs[3].append(row[4])
-		rs[4].append(row[5])
-		rs[5].append(row[6])
-		rs[6].append(row[7])
-		rs[7].append(row[8])
-		rs[8].append(row[9])
-		rs[9].append(row[10])
-		rs[10].append(row[11])
-		rs[11].append(row[12])
-		rs[12].append(row[13])
-		rs[13].append(row[14])
-		rs[14].append(row[15])
-		rs[15].append(row[16])
-		rs[16].append(row[17])
+		times.append(row[0])
+		rs[1].append(row[1])
+		rs[2].append(row[2])
+		rs[3].append(row[3])
+		rs[4].append(row[4])
+		rs[5].append(row[5])
+		rs[6].append(row[6])
+		rs[7].append(row[7])
+		rs[8].append(row[8])
+		rs[9].append(row[9])
+		rs[10].append(row[10])
+		rs[11].append(row[11])
+		rs[12].append(row[12])
+		rs[13].append(row[13])
+		rs[14].append(row[14])
+		rs[15].append(row[15])
+		rs[16].append(row[16])
+	return times,rs
+
+def checkRainSensor(tlim):
+	times,rs=getLastXhrs(tlim)
 	for i in rs:
 		if 0 in rs[i]:
 			print "Faulty Sensor: %d" % (i)
 
+def plotRainSensor(tlim):
+	times,rs=getLastXhrs(tlim)
+	fig=pl.figure(1,figsize=(5,5))
+	ax=pl.subplot2grid((5,4),(0,0),colspan=5,rowspan=4)
+	ax.set_color_cycle(colours)
+	ax.set_xlim(-24,0)
+	ax.set_ylim(0,1.2)
+	for i in rs:
+		ax.plot(times,rs[i],'-')
+	ax.set_xlabel('Hours ago')
+	ax.set_ylabel('Rain (0=Wet, 1=Dry)')
+	pl.savefig('/home/ops/ngts/prism/monitor/img/rpi_rain_sensor.png',bbox_inches='tight')
+
+
 if __name__=="__main__":
-	checkRainSensor()
+	args=argParse()
+	if args.check: 
+		checkRainSensor(args.check)
+	if args.plot:
+		plotRainSensor(args.plot)
