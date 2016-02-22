fa420ad0ed1f766d6345f2ae18fd6d002052b9f4
James McCormac <jmccormac001"gmail.com>
2016-02-22 13:08:01 +0000

    Correct imports

--- rainSensor-server.py
+++ rainSensor-server.py
@@ -6,6 +6,8 @@ import Pyro4
 from collections import defaultdict
 import RPi.GPIO as g
 import time
+import datetime
+import pymysql
 
 SLEEP_TIME = 5
 
@@ -37,7 +39,7 @@ def update_rain_info(sensor, time_value):
 
 	rain_values = sensor.get_rain('test')
 	bucket=(int(time_value)/60)*60
-	tsample=datetime.utcnow().isoformat().replace('T',' ')[:-7] # remove microseconds
+	tsample=datetime.datetime.utcnow().isoformat().replace('T',' ')[:-7] # remove microseconds
 
 	qry= "REPLACE INTO rpi_rain_sensor (tsample,bucket,rs01,rs02,rs03,rs04,rs05,rs06,rs07,rs08,rs09,rs10,rs11,rs12,rs13,rs14,rs15,rs16) VALUES ('%s',%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)"
 	params = (tsample,bucket,rs[1],rs[2],rs[3],rs[4],rs[5],rs[6],rs[7],rs[8],rs[9],rs[10],rs[11],rs[12],rs[13],rs[14],rs[15],rs[16])
