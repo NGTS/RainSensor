1365fef741b36f7a25aa51861375db2d736cefd7
James McCormac <jmccormac001@gmail.com>
2016-12-09 11:15:54 +0000

    tidying up rain sensor code for move to warwick

--- checkRainSensor.py
+++ checkRainSensor.py
@@ -1,37 +1,60 @@
 #!/usr/local/python/bin/python
-import pymysql
-from collections import defaultdict
-from astropy.time import Time
-import matplotlib.pyplot as pl
+"""
+Plot the current status from the RPi rain sensors
+"""
 import argparse as ap
+from collections import defaultdict
+import pymysql
+import matplotlib.pyplot as pl
 import numpy as np
 
-colours=[(206/250.,200/250.,170/250.),
-        (203/250.,81/250.,205/250.),
-        (213/250.,89/250.,52/250.),
-        (135/250.,215/250.,83/250.),
-        (210/250.,76/250.,121/250.),
-        (208/250.,180/250.,70/250.),
-        (99/250.,140/250.,75/250.),
-        (128/250.,121/250.,213/250.),
-        (120/250.,153/250.,179/250.),
-        (167/250.,118/250.,92/250.),
-        (120/250.,214/250.,181/250.),
-        (193/250.,128/250.,174/250.)]
+# pylint: disable = invalid-name
+# pylint: disable = redefined-outer-name
+
+DB_HOST = 'ngtsdb'
+DB_DATABASE = 'ngts_ops'
+colours = [(206/250., 200/250., 170/250.),
+           (203/250., 81/250., 205/250.),
+           (213/250., 89/250., 52/250.),
+           (135/250., 215/250., 83/250.),
+           (210/250., 76/250., 121/250.),
+           (208/250., 180/250., 70/250.),
+           (99/250., 140/250., 75/250.),
+           (128/250., 121/250., 213/250.),
+           (120/250., 153/250., 179/250.),
+           (167/250., 118/250., 92/250.),
+           (120/250., 214/250., 181/250.),
+           (193/250., 128/250., 174/250.)]
 
 def argParse():
-    description='''Script to check the rain sensor calibration and plot the sensor values'''
-    parser=ap.ArgumentParser(description=description)
-    parser.add_argument('--check',type=int,help="check the sensors for last X hours")
-    parser.add_argument('--plot',type=int,help="plot sensor values for last X hours")
+    """
+    Parse the commmand line arguments
+    """
+    description = """Script to check the rain sensor calibration and plot the sensor values"""
+    parser = ap.ArgumentParser(description=description)
+    parser.add_argument('--check',
+                        type=int,
+                        help="check the sensors for last X hours")
+    parser.add_argument('--plot',
+                        type=int,
+                        help="plot sensor values for last X hours")
     return parser.parse_args()
 
 def getLastXhrs(tlim):
-    conn=pymysql.connect(host='ds',db='ngts_ops')
-    cur=conn.cursor()
-    rs=defaultdict(list)
-    times=[]
-    qry="SELECT (bucket-UNIX_TIMESTAMP())/3600.0 AS trel,rs01,rs02,rs03,rs04,rs05,rs06,rs07,rs08,rs09,rs10,rs11,rs12,rs13,rs14,rs15,rs16 FROM rpi_rain_sensor HAVING trel>-%d" % (tlim)
+    """
+    Grab the last X hours of rain sensor data
+    """
+    conn = pymysql.connect(host=DB_HOST, db=DB_DATABASE)
+    cur = conn.cursor()
+    rs = defaultdict(list)
+    times = []
+    qry = """
+        SELECT (bucket-UNIX_TIMESTAMP())/3600.0 AS trel,
+        rs01, rs02, rs03, rs04, rs05, rs06, rs07, rs08,
+        rs09, rs10, rs11, rs12, rs13, rs14, rs15, rs16
+        FROM rpi_rain_sensor
+        HAVING trel>-{}
+        """.format(tlim)
     cur.execute(qry)
     for row in cur:
         times.append(row[0])
@@ -51,35 +74,44 @@ def getLastXhrs(tlim):
         rs[14].append(row[14])
         rs[15].append(row[15])
         rs[16].append(row[16])
-    return times,rs
+    return times, rs
 
 def checkRainSensor(tlim):
-    times,rs=getLastXhrs(tlim)
+    """
+    Check the outputs of the rain sensors
+    """
+    _, rs = getLastXhrs(tlim)
     for i in rs:
         if 0 in rs[i]:
-            print "Faulty Sensor: %d" % (i)
+            print("Faulty Sensor: {}".format(i))
 
-def plotRainSensor(tlim):
-    times,rs=getLastXhrs(tlim)
-    fig=pl.figure(1,figsize=(5,5))
-    ax=pl.subplot2grid((5,4),(0,0),colspan=5,rowspan=4)
+def plotRainSensor(outdir, tlim):
+    """
+    Make a plot of the 16 rain sensors
+    """
+    times, rs = getLastXhrs(tlim)
+    pl.figure(1, figsize=(5, 5))
+    ax = pl.subplot2grid((5, 4), (0, 0), colspan=5, rowspan=4)
     ax.set_color_cycle(colours)
-    ax.set_xlim(-24,5.2)
-    ax.set_ylim(0,1.1)
+    ax.set_xlim(-24, 5.2)
+    ax.set_ylim(0, 1.1)
     for i in rs:
-        ax.plot(times,np.abs((np.array(rs[i])-1)+(i*0.003)),'-')
+        ax.plot(times, np.abs((np.array(rs[i])-1)+(i*0.003)), '-')
     ax.set_xlabel('Hours ago')
     ax.set_ylabel('Rain (0=DRY, 1=WET)')
-    pl.rc('legend',**{'fontsize':9})
-    pl.legend(("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16"),loc='upper right',numpoints=1)
-    pl.savefig('/home/ops/ngts/prism/monitor/img/rpi_rain_sensor.png',bbox_inches='tight')
+    pl.rc('legend', **{'fontsize':9})
+    pl.legend(("01", "02", "03", "04", "05", "06", "07", "08",
+               "09", "10", "11", "12", "13", "14", "15", "16"),
+              loc='upper right', numpoints=1)
+    pl.savefig('{}/rpi_rain_sensor.png'.format(outdir),
+               bbox_inches='tight')
 
 
-if __name__=="__main__":
-    args=argParse()
-    if args.check: 
+if __name__ == "__main__":
+    args = argParse()
+    if args.check:
         checkRainSensor(args.check)
     if args.plot:
-        plotRainSensor(args.plot)
-
+        outdir = "/srv/www/ngts/monitor/monitor/static"
+        plotRainSensor(outdir, args.plot)
 
--- rainsensor.py
+++ rainsensor.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
-# start name server with
-# python -m Pyro4.naming -n 10.2.5.107
+"""
+Raspberry Pi rain sensors
 
-import Pyro4
-from collections import defaultdict
-import RPi.GPIO as g
+start name server with:
+python -m Pyro4.naming -n 10.2.5.107
+"""
 import time
 import datetime
-import pymysql
 import argparse
 import logging
+from collections import defaultdict
+import Pyro4
+import RPi.GPIO as g
+import pymysql
+
+# pylint: disable = invalid-name
 
 SLEEP_TIME = 5
 
@@ -18,11 +23,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 class RainSensor(object):
-    
+    """
+    Rain sensor class
+    """
     def __init__(self):
         """Sets up the output dict and GPIO pin mode"""
         self._running = True
-        self.rs=defaultdict(list)
+        self.rs = defaultdict(list)
         g.setmode(g.BCM)
 
     def running(self):
@@ -31,32 +38,48 @@ class RainSensor(object):
 
     def get_rain(self, name):
         """Returns the rain sensor values in a dict"""
-        gpio_nums=[18,23,4,17,27,22,10,9,11,5,6,13,19,26,20,21]
+        gpio_nums = [18, 23, 4, 17, 27,
+                     22, 10, 9, 11, 5, 6,
+                     13, 19, 26, 20, 21]
         for i in gpio_nums:
-            g.setup(i,g.IN)
-        for i in range(0,len(gpio_nums)):
-            self.rs[(i+1)]=g.input(gpio_nums[i])
+            g.setup(i, g.IN)
+        for i in range(0, len(gpio_nums)):
+            self.rs[(i+1)] = g.input(gpio_nums[i])
         return self.rs
 
 def update_rain_info(sensor, time_value):
+    """
+    Log the rain sensor information
+    """
     host = '10.2.5.32'
     db = 'ngts_ops'
     user = 'ops'
-
     logger.debug('Fetching rain sensor measurements')
     rs = sensor.get_rain('test')
-    bucket=(int(time_value)/60)*60
-    tsample=datetime.datetime.utcnow().isoformat().replace('T',' ')[:-7] # remove microseconds
+    bucket = (int(time_value)/60)*60
+    tsample = datetime.datetime.utcnow().isoformat().replace('T', ' ')[:-7] # remove microseconds
 
-    qry= "REPLACE INTO rpi_rain_sensor (tsample,bucket,rs01,rs02,rs03,rs04,rs05,rs06,rs07,rs08,rs09,rs10,rs11,rs12,rs13,rs14,rs15,rs16) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    params = (tsample,bucket,rs[1],rs[2],rs[3],rs[4],rs[5],rs[6],rs[7],rs[8],rs[9],rs[10],rs[11],rs[12],rs[13],rs[14],rs[15],rs[16])
-
+    qry = """
+         REPLACE INTO rpi_rain_sensor
+         (tsample, bucket, rs01, rs02, rs03, rs04, rs05,
+         rs06, rs07, rs08, rs09, rs10, rs11, rs12, rs13,
+         rs14, rs15, rs16)
+         VALUES
+         (%s, %s, %s, %s, %s, %s, %s, %s, %s,
+         %s, %s, %s, %s, %s, %s, %s ,%s, %s)
+         """
+    params = (tsample, bucket, rs[1], rs[2], rs[3], rs[4], rs[5], rs[6],
+              rs[7], rs[8], rs[9], rs[10], rs[11], rs[12], rs[13], rs[14],
+              rs[15], rs[16])
     with pymysql.connect(host=host, db=db, user=user) as cursor:
         logger.debug('Updating database: %s : %s', qry, params)
         cursor.execute(qry, params)
         # implicit commit
 
 def rain_sensor_watcher(sensor):
+    """
+    Set up the rain sensor watcher
+    """
     # Connect to central hub
     logger.debug('Connecting to central hub')
     hub = Pyro4.Proxy('PYRONAME:central.hub')
@@ -70,16 +93,18 @@ def rain_sensor_watcher(sensor):
         time.sleep(SLEEP_TIME)
 
 
-def get_args():
+def argParse():
+    """
+    Parse the command line arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
 if __name__ == '__main__':
-    args = get_args()
+    args = argParse()
     if args.verbose:
         logger.setLevel('DEBUG')
     sensor = RainSensor()
     rain_sensor_watcher(sensor)
 
-# vim: set noexpandtab ts=4 sw=4:
